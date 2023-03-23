2d56a74821718f73d41dc2f63a78d307fccd5330
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-03-24 00:18:15 +0530

    changess

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-alert("hello")
+const header = document.querySelector("header");
+// =================sticky Navbar=============
+function stickyNavbar(){
+   header.classList.toggle("scrolled",window.pageYOffset>0);
+// console.log(window.pageYOffset > 0)
+}
+stickyNavbar();
+window.addEventListener("scroll",stickyNavbar);
