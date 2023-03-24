bc7cc9b95dab51d3cb5727d42006c511814db15d
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-03-25 00:28:24 +0530

    changess

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ function stickyNavbar(){
 }
 stickyNavbar();
 window.addEventListener("scroll",stickyNavbar);
+// reveal
+let sr =ScrollReveal(
+   {
+      duration:2500,
+      distance: "60px"
+   }
+);
+
+sr.reveal(".showcase-info",{delay:600})
+sr.reveal(".showcase-image",{origin:"top", delay:700})
